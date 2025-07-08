6e1d068dab6d9e67eae261382927df5d8c79ab21
myltx <summmayl@gmail.com>
2025-07-08 16:20:51 +0800

    feat: 更新 GiscusComments 组件，调整评论映射方式并添加路径变化时的重新渲染

--- src/components/GiscusComments.tsx
+++ src/components/GiscusComments.tsx
@@ -1,13 +1,16 @@
+import { useLocation } from "react-router-dom";
 import Giscus from "@giscus/react";
 
 const GiscusComments = () => {
+  const location = useLocation();
   return (
     <Giscus
       repo={import.meta.env.VITE_REACT_APP_GISCUS_REPO}
       repoId={import.meta.env.VITE_REACT_APP_GISCUS_REPO_ID}
       category={import.meta.env.VITE_REACT_APP_GISCUS_CATEGORY}
       categoryId={import.meta.env.VITE_REACT_APP_GISCUS_CATEGORY_ID}
-      mapping="title"
+      mapping="pathname"
+      key={location.pathname} // 确保路径变化时重新渲染
       reactions-enabled="1"
       emit-metadata="1"
       input-position="top"
