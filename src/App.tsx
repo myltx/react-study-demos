03b2dc7fd8c5f17483fbcf29e894e6f08cf9d2e4
myltx <summmayl@gmail.com>
2025-07-08 16:56:27 +0800

    feat: ✨ 替换BrowserRouter为HashRouter并更新基础路径

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import MainLayout from "./layout/MainLayout";
 
 document.title = import.meta.env.VITE_APP_TITLE || "默认标题";
+
 const App: React.FC = () => (
-  <BrowserRouter basename={import.meta.env.BASE_URL}>
+  <HashRouter>
     <MainLayout />
-  </BrowserRouter>
+  </HashRouter>
 );
 
 export default App;
